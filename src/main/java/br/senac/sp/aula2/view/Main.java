7faeb7047f72ee3122b09b8850d3b33df1a4a815
ROGERIO SARTORI LUCON <rogerio.slucon@senacsp.edu.br>
2019-08-29 11:26:08 -0300

    Maximized movido pra classe main

--- src/main/java/br/senac/sp/aula2/view/Main.java
+++ src/main/java/br/senac/sp/aula2/view/Main.java
@@ -5,6 +5,12 @@
  */
 package br.senac.sp.aula2.view;
 
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author fernando.fernandes
@@ -15,7 +21,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrameCustomizadoView jf = new JFrameCustomizadoView();
+                try {
+                    UIManager.setLookAndFeel(new WindowsLookAndFeel());
+                } catch (UnsupportedLookAndFeelException ex) {
+                    Logger.getLogger(JFrameCustomizadoView.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                jf.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+                jf.setVisible(true);
     }
     
 }
